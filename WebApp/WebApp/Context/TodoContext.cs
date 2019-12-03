cf03aad176ea05180e2d9d4596aceafd336c6e11
rameshksh <ramesh.sharma12@gmail.com>
2019-12-03 18:05:49 +0530

    Initial project

---
+++ WebApp/WebApp/Context/TodoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Context
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+    }
+}
